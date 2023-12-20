cb72039dc73c4b5603e21d7364531419b6114580
Edward-Kabue <ekabue0@gmail.com>
2023-12-20 15:44:35 +0300

    Scaffold for repository pattern

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/ist/android/words/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ist.android.words.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.raywenderlich.android.words.test";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ app/src/main/kotlin/com/ist/android/words/WordsApp.kt
@@ -0,0 +1,7 @@
+package com.ist.android.words
+
+import android.app.Application
+
+class WordsApp : Application() {
+
+}
